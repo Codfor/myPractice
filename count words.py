7d8a0b7ba7a33dfdfce330877be15bb405070f90
codfor <forumes@hotmail.com>
2021-04-18 13:52:40 +0400

    18th april practices

---
+++ count words.py
@@ -0,0 +1,55 @@
+my_file = open('count words.txt')
+read_file = my_file.read()
+
+# to read all text in the file line by line
+# for w in my_file :
+#     print(w)
+
+# to read all text in the file line by line while taking the new line out at the end of each line
+# for w in my_file :
+#     print(w.rstrip())
+
+# # find a word 'tickets' from the file
+# first_word = read_file.find('tickets')
+# last_word = read_file.find('/',334) # to find '/' after the word 'tickets
+# print(first_word)
+# print(last_word)
+# full_word = read_file[first_word:last_word] # to split word 'ticket' from entire text file
+# print(full_word)
+
+
+# create a list without spaces from the text file
+# the_list = list()
+# split_words = read_file.split() # to split the text file into word by word
+#
+# for w in split_words:
+#     if w == ' ':
+#         continue
+#     the_list.append(w) # add the word if its not blank space to the list
+# print(the_list)
+
+# ##create a list without spaces from the text file
+# the_list = list()
+# split_words = read_file.split() # to split the text file into word by word
+
+# for w in split_words:
+#     if w == ' ':
+#         continue
+#     the_list.append(w) # add the word if its not blank space to the list
+# print(the_list.count('to')) # cont how many time word 'to' in the file
+
+
+the_list = list()
+split_words = read_file.split() # to split the text file into word by word
+dic_list =dict()
+print(split_words)
+for w in split_words:
+    if w not in dic_list: # if the word is not in the dictionary the added it
+        dic_list[w] = 1
+    else: # otherwise add 1 to it to count how many times it wad mentioned in the file
+        dic_list[w]= dic_list[w]+1
+print(dic_list)
+
+
+
+
